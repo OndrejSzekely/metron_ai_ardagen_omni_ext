b8d8208f62a89399a2d9d3aaa3078b6b278a5347
ondra <ondra.szekely@gmail.com>
2022-12-31 10:44:07 +0100

    refa: Refactor `tools` scripts

--- tools/packman/bootstrap/install_package.py
+++ tools/packman/bootstrap/install_package.py
@@ -1,3 +1,6 @@
+"""
+Install Package module.
+"""
 # Copyright 2019 NVIDIA CORPORATION
 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,6 +20,7 @@ import zipfile
 import tempfile
 import sys
 import shutil
+import typing
 
 __author__ = "hfannar"
 logging.basicConfig(level=logging.WARNING, format="%(message)s")
@@ -24,34 +28,41 @@ logger = logging.getLogger("install_package")
 
 
 class TemporaryDirectory:
-    def __init__(self):
-        self.path = None
+    """
+    Temporary Directory.
+    """
 
-    def __enter__(self):
+    def __init__(self) -> None:
+        self.path: str = ""
+
+    def __enter__(self) -> str:
         self.path = tempfile.mkdtemp()
         return self.path
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, type_arg: typing.Any, value: typing.Any, traceback: typing.Any) -> None:
         # Remove temporary data created
         shutil.rmtree(self.path)
 
 
-def install_package(package_src_path, package_dst_path):
-    with zipfile.ZipFile(
-        package_src_path, allowZip64=True
-    ) as zip_file, TemporaryDirectory() as temp_dir:
+def install_package(package_src_path: str, package_dst_path: str) -> None:
+    """
+    Installs given package.
+
+    Args:
+        package_src_path (_type_): _description_
+        package_dst_path (_type_): _description_
+    """
+    with zipfile.ZipFile(package_src_path, allowZip64=True) as zip_file, TemporaryDirectory() as temp_dir:
         zip_file.extractall(temp_dir)
         # Recursively copy (temp_dir will be automatically cleaned up on exit)
         try:
             # Recursive copy is needed because both package name and version folder could be missing in
             # target directory:
             shutil.copytree(temp_dir, package_dst_path)
-        except OSError as exc:
-            logger.warning(
-                "Directory %s already present, packaged installation aborted" % package_dst_path
-            )
+        except OSError:
+            logger.warning("Directory %s already present, packaged installation aborted", package_dst_path)
         else:
-            logger.info("Package successfully installed to %s" % package_dst_path)
+            logger.info("Package successfully installed to %s", package_dst_path)
 
 
 install_package(sys.argv[1], sys.argv[2])
